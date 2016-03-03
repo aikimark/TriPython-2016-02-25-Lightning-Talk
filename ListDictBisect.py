57c59688aeec9e66db29be43820a9ab528bc21af
aikimark <aikimark@aol.com>
2016-03-02 22:36:54 -0500

    Include Set data structure test
    From audience feedback, I've added  a Set() version of the stopwords
    list.  We have a new performance winner.

--- ListDictBisect.py
+++ ListDictBisect.py
@@ -29,7 +29,7 @@ def main():
     stopwordsbyfreq = ['the','of','and','a','to','in','is','be','that','was','he','for','it','with','as','his','i','on','have','at','by','not','they','this','had','are','but','from','or','she','an','which','you','we','all','were','her','would','there','their','will','when','who','him','been','has','if','no','do','so','can','what','said','about','other','into','than','its','only','could','them','some','these','then','may','any','like','my','our','most','me','after','also','did','must','where','get','your','should','because','just','say','how','own','too','us','while','might','off','since','however','does','got','every','almost','let','yet','why','rather','among','often','ever','least','am','either','cannot','across','able','says','nor','else','likely','neither','whom','wants','dear','hers','tis','twas']
     #stopwordsDic are the (English) stop words in a dictionary
     stopwordsDic = {'a' : 1,'able' : 1,'about' : 1,'across' : 1,'after' : 1,'all' : 1,'almost' : 1,'also' : 1,'am' : 1,'among' : 1,'an' : 1,'and' : 1,'any' : 1,'are' : 1,'as' : 1,'at' : 1,'be' : 1,'because' : 1,'been' : 1,'but' : 1,'by' : 1,'can' : 1,'cannot' : 1,'could' : 1,'dear' : 1,'did' : 1,'do' : 1,'does' : 1,'either' : 1,'else' : 1,'ever' : 1,'every' : 1,'for' : 1,'from' : 1,'get' : 1,'got' : 1,'had' : 1,'has' : 1,'have' : 1,'he' : 1,'her' : 1,'hers' : 1,'him' : 1,'his' : 1,'how' : 1,'however' : 1,'i' : 1,'if' : 1,'in' : 1,'into' : 1,'is' : 1,'it' : 1,'its' : 1,'just' : 1,'least' : 1,'let' : 1,'like' : 1,'likely' : 1,'may' : 1,'me' : 1,'might' : 1,'most' : 1,'must' : 1,'my' : 1,'neither' : 1,'no' : 1,'nor' : 1,'not' : 1,'of' : 1,'off' : 1,'often' : 1,'on' : 1,'only' : 1,'or' : 1,'other' : 1,'our' : 1,'own' : 1,'rather' : 1,'said' : 1,'say' : 1,'says' : 1,'she' : 1,'should' : 1,'since' : 1,'so' : 1,'some' : 1,'than' : 1,'that' : 1,'the' : 1,'their' : 1,'them' : 1,'then' : 1,'there' : 1,'these' : 1,'they' : 1,'this' : 1,'tis' : 1,'to' : 1,'too' : 1,'twas' : 1,'us' : 1,'wants' : 1,'was' : 1,'we' : 1,'were' : 1,'what' : 1,'when' : 1,'where' : 1,'which' : 1,'while' : 1,'who' : 1,'whom' : 1,'why' : 1,'will' : 1,'with' : 1,'would' : 1,'yet' : 1,'you' : 1,'your' : 1}
-
+    stopwordsSet = set(stopwords)
 #    f=file("c:\users\mark\documents\GettysburgAddress.txt",'r')
     f=file(".\GettysburgAddress.txt",'r')
     lines= f.readlines()
@@ -79,5 +79,16 @@ def main():
             methodtime += timeit.default_timer() - start
     print "stopwordsInlist\t", methodtime, "\t", nfcount
 
+    nfcount=0
+    methodtime=0
+    for line in lines:
+        for word in line.split(" "):
+            start = timeit.default_timer()
+            if word not in stopwordsSet:
+                nfcount += 1
+            methodtime += timeit.default_timer() - start
+    print "stopwordsSet\t", methodtime, "\t", nfcount
+
 if __name__ == '__main__':
-    main()
+##    for i in xrange(30):
+        main()
--- README.md
+++ README.md
@@ -12,13 +12,15 @@ I ran the code a couple of dozen times.
 
 Method|Min|Avg|Median|Max
 ------|---|---|------|---
-stopwords|0.001056|0.001084|0.001061|0.001275
-stopwordsbyfreq|0.000904|0.000925|0.000910|0.001007
-stopwordsDic|0.000461|0.000467|0.000464|0.000541
-stopwordsInlist|0.000855|0.000889|0.000865|0.001205
+stopwords|0.001045|0.001064|0.001052|0.001272
+stopwordsbyfreq|0.000912|0.001272|0.000957|0.002249
+stopwordsDic|0.000462|0.000500|0.000505|0.000590
+stopwordsInlist|0.000836|0.000968|0.000886|0.002135
+stopwordsSet|0.000420|0.000654|0.000462|0.001750
 
 ### The performance winners are: ###
-1. Dictionary
-2. Binary search
-3. list ordered by expected occurance (frequency)
-4. a list in any order
+1. Set
+2. Dictionary
+3. Binary search
+4. list ordered by expected occurance (frequency)
+5. a list in any order
